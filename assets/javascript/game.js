35ada5373e8d407680076209516c0d344e507c97
Tony Carnese <Tony Carnese>
2016-11-30 14:23:32 -0500

    data events with crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,62 @@
-var targetNumber = 53;
+var targetNumber = Math.floor(Math.random()*50 + 20)
 var counter = 0;
 var numberOptions = [10, 5, 3, 7];
-  
+var wins = 0;
+var lose = 0;
+var audioElement = document.createElement('audio');
+// var button = $("<button type = button>").addClass("btn", "btn-success").html("New Game");
 // alert("hi");
 
 $("#total").append(targetNumber);
+$("#soFar").append(counter);
+
+
+// trying to associate data value to #crystals from numberOptions or randomly
+
+// $('#crystals').attr('data-val', Math.floor(Math.random()*30));
+
+
+// $("#crystals").data("crystalValue", "Math.ceil(Math.random()*11)")
 
 //  for (var i = 0; i < numberOptions.length; i++) {
 // 	$("<img>").attr("data-crystalvalue", "numberOptions[Math.floor(Math.random() * numberOptions.length)]");
 // }
-  for (var i = 0; i < numberOptions.length; i++) {
+//   for (var i = 0; i < numberOptions.length; i++) {
+// $("#crystals").attr('data-val',"numberOptions[i].random");
+    
+// }
 
-    // trying to associate data value to #crystals from numberOptions
-    var imageCrystal = $("#crystals").attr("data-crystalvalue","numberOptions[i].random");
+
+function initNewGame(){
+    // resets game variables
+    targetNumber = Math.floor(Math.random()*50 + 20);
+	counter = 0;
+	$("#total").html(targetNumber);
+	$("#soFar").html(counter);
 }
 
+
+// When clicking on crystals...
 $(".crystals").on("click", function() {
 	var crystalValue = ($(this).data("crystalvalue"));
 	counter = counter + crystalValue;
-	console.log("New score: " + counter);
+	// console.log("New score: " + counter);
+	$("#soFar").html(counter);
+
 if (counter === targetNumber) {
-      console.log("You win!");
-        }
+      // console.log("You win!");
+      $("#score").html("You Won!");
+      $("#score").append(button);
+      wins++;
+      initNewGame();
+  }
 
     else if (counter > targetNumber) {
-      console.log("You lose!!");
+      // console.log("You lose!!");
+      $("#score").html("You lose!");
+      lose++;
+      initNewGame();
+     
     }
 
   });
