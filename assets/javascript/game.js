606b76d85909bd6c7878b75a8473871930e61e96
Tony Carnese <Tony Carnese>
2016-12-05 18:39:27 -0500

    fixed jQuery link as per Instructor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
-var targetNumber = Math.floor(Math.random()*50 + 20)
+var targetNumber = Math.floor(Math.random()*50 + 20);
 var counter = 0;
 var numberOptions = [10, 5, 3, 7];
 var wins = 0;
 var lose = 0;
-var audioElement = document.createElement('audio');
+// var audioElement = document.createElement('audio');
 // var button = $("<button type = button>").addClass("btn", "btn-success").html("New Game");
 // alert("hi");
 
@@ -13,18 +13,6 @@ $("#soFar").append(counter);
 
 // trying to associate data value to #crystals from numberOptions or randomly
 
-// $('#crystals').attr('data-val', Math.floor(Math.random()*30));
-
-
-// $("#crystals").data("crystalValue", "Math.ceil(Math.random()*11)")
-
-//  for (var i = 0; i < numberOptions.length; i++) {
-// 	$("<img>").attr("data-crystalvalue", "numberOptions[Math.floor(Math.random() * numberOptions.length)]");
-// }
-//   for (var i = 0; i < numberOptions.length; i++) {
-// $("#crystals").attr('data-val',"numberOptions[i].random");
-    
-// }
 
 
 function initNewGame(){
@@ -33,6 +21,8 @@ function initNewGame(){
 	counter = 0;
 	$("#total").html(targetNumber);
 	$("#soFar").html(counter);
+  // $('.crystals').attr('data-crystalvalue', Math.floor(Math.random()*30));
+
 }
 
 
@@ -40,21 +30,21 @@ function initNewGame(){
 $(".crystals").on("click", function() {
 	var crystalValue = ($(this).data("crystalvalue"));
 	counter = counter + crystalValue;
-	// console.log("New score: " + counter);
+	console.log("New score: " + counter);
 	$("#soFar").html(counter);
+
 
 if (counter === targetNumber) {
       // console.log("You win!");
       $("#score").html("You Won!");
-      $("#score").append(button);
-      wins++;
+       wins++;
+      $("#saveMe").append(wins);
       initNewGame();
   }
 
     else if (counter > targetNumber) {
       // console.log("You lose!!");
       $("#score").html("You lose!");
-      lose++;
       initNewGame();
      
     }
